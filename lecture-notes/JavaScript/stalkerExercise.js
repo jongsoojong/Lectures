9ef2cf3083c5d20a781d3265196d47eaa1f7e864
timeexe <user@Users-MBP.attlocal.net>
2020-01-30 11:18:33 -0800

    First chunk of JavaScript Lessons

---
+++ lecture-notes/JavaScript/stalkerExercise.js
@@ -0,0 +1,5 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+console.log("Nice to meet you " + userFirstName + " " + userLastName);
+console.log("You are " + userAge + " years old");
